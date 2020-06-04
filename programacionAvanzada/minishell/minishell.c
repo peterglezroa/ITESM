defd8884e18f9da8f685219cc3350f5d758fa5f4
peterglezroa <peterglezroa@gmail.com>
2020-06-03 20:33:32 -0500

    Progra: Avance minishell - Interactive mode  On branch progra/proyecto  Changes to be committed: 	modified:   minishell 	modified:   minishell.c

--- programacionAvanzada/minishell/minishell.c
+++ programacionAvanzada/minishell/minishell.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <pwd.h>
@@ -19,7 +20,7 @@
 
 #define MAX_BUFFER 512
 
-void parse_command(char *string){
+void exec_command(char *string){
   int argc = 1, i = 0, str_size, reading = 1;
   char **argv, *pointer1, *pointer2;
   // Contar cantidad de argumentos
@@ -31,24 +32,24 @@ void parse_command(char *string){
   argv = malloc(sizeof(char *)*argc);
   // Obtener commando y argumento
   pointer2 = string;
-  for(pointer1 = string; 1 == 1; pointer1++){
-    if( *pointer1 == ' ' || *pointer1 == '\0'){
+  for(pointer1 = string; *pointer1 != '\0'; pointer1++){
+    if( *pointer1 == ' ' || *pointer1 == '\n' ){
       str_size = pointer1-pointer2;
-      argv[i] = malloc(sizeof(char)*str_size+1);
-      strncpy(argv[i], pointer2, str_size);
-      argv[i][str_size] = '\0';
-      fprintf(stdout, "argumento %i: %s\n", i, argv[i]);
-      i++;
-
-      if( *pointer1=='\0' ) // Terminar for
-        break;
-
+      if(str_size > 0){
+        argv[i] = malloc(sizeof(char)*str_size+1);
+        strncpy(argv[i], pointer2, str_size);
+        argv[i][str_size] = '\0';
+//        fprintf(stdout, "argumento %i: %s\n", i, argv[i]);
+        i++;
+      }
       pointer1++;
       pointer2=pointer1;
     }
   }
   // Correr commando
-  execvp(argv[0], argv);
+  if( execvp(argv[0], argv) != 0){
+    fprintf(stdout, "Command %s execute error\n", argv[0]);
+  }
   
   // Liberar memoria
   for( int i = 0; i < argc; i++){
@@ -56,7 +57,36 @@ void parse_command(char *string){
   }
 }
 
-void iteractive_mode(){
+void parse_run_commands(char *input){
+  char *command, *pointer1, *pointer2;
+  int pid, fork_count = 0, str_size;
+  pointer2 = input;
+  for(pointer1=input; *pointer1 != '\0'; pointer1++){
+    if( *pointer1 == ';' || *pointer1 == '\n' ){
+      str_size = pointer1-pointer2;
+      command = malloc(sizeof(char)*str_size+2);
+      strncpy(command, pointer2, str_size);
+      command[str_size] = '\n';
+      command[str_size+1] = '\0';
+      
+      // Make fork for command
+      if( (pid=fork()) == 0){
+        exec_command(command);
+        free(command);
+        exit(0);
+      }
+      fork_count++;
+      pointer1++;
+      pointer2=pointer1;
+    }
+  }
+  while(fork_count > 0){
+    wait(NULL);
+    fork_count--;
+  }
+}
+
+void interactive_mode(){
   int running = 1;
   char buffer[512];
   while(running){
@@ -67,13 +97,13 @@ void iteractive_mode(){
       fprintf(stderr, "Error al leer commando.\n");
       exit(-2);
     }
-    fprintf(stdout, "%s", buffer);
+//    fprintf(stdout, "%s", buffer);
 
-    if( strcmp(buffer, "quit") == 0 ){ // Quit command
-      fprintf(stdout,"Quitting shell");
+    if( strcmp(buffer, "quit\n") == 0 ){ // Quit command
+      fprintf(stdout,"Quitting shell...\n");
       running = 0;
-    }else{
-      parse_command(buffer);
+    }else if( buffer[0] != '\n' ){
+      parse_run_commands(buffer);
     }
   }
 }
@@ -86,8 +116,8 @@ int main(int argc, char *argv[]){
   }
 
   if(argc == 1){ // Modo iteractivo
-    fprintf(stdout, "Entering iteractive mode...\n");
-    iteractive_mode();
+    fprintf(stdout, "Entering interactive mode...\n");
+    interactive_mode();
   }else{ // Modo lote
 
   }
